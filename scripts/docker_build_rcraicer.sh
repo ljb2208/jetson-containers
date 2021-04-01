be6458a2fc1807d727be0a8e3f1cc6346c463b46
Luke Barnett <ljb2208@gmail.com>
2021-04-01 12:42:23 -0400

    Changes to support rcraicer image

---
+++ scripts/docker_build_rcraicer.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+L4T_VERSION="r32.4.4"
+ROS_DISTRO="foxy"
+BASE_IMAGE="ros:$ROS_DISTRO-ros-base-l4t-$L4T_VERSION"
+
+echo "building container for rcraicer..."
+echo "base image..." $BASE_IMAGE 
+
+TO_BUILD=($ROS_DISTRO)
+
+sh ./scripts/docker_build.sh  Dockerfile.rcraicer --build-arg BASE_IMAGE=$BASE_IMAGE
