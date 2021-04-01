f82c783c315d98b67172869ebba1be577490368c
Luke Barnett <ljb2208@gmail.com>
2021-04-01 15:23:28 -0400

    Add new latest files and fixes for base files

--- scripts/docker_build_rcraicer_latest.sh
+++ scripts/docker_build_rcraicer_latest.sh
@@ -5,11 +5,11 @@ set -e
 
 L4T_VERSION="r32.4.4"
 ROS_DISTRO="foxy"
-BASE_IMAGE="ros:$ROS_DISTRO-ros-base-l4t-$L4T_VERSION"
+BASE_IMAGE="ros:rcraicer-base"
 
 echo "building container for rcraicer..."
 echo "base image..." $BASE_IMAGE 
 
 TO_BUILD=($ROS_DISTRO)
 
-sh ./scripts/docker_build.sh ros:rcraicer-base Dockerfile.rcraicer.base --build-arg BASE_IMAGE=$BASE_IMAGE
+sh ./scripts/docker_build.sh ros:rcraicer-latest Dockerfile.rcraicer.latest --build-arg BASE_IMAGE=$BASE_IMAGE
