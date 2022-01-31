fcf7a9da2c9aca393ccfd5617248b3110850b1d0
Luke Barnett <ljb2208@gmail.com>
2022-01-31 10:56:46 -0500

    Add new files

---
+++ scripts/docker_build_rcraicer.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+L4T_VERSION="r32.6.1"
+ROS_DISTRO="foxy"
+BASE_IMAGE="ros:$ROS_DISTRO-ros-base-l4t-$L4T_VERSION"
+
+echo "building container for rcraicer..."
+echo "base image..." $BASE_IMAGE 
+
+TO_BUILD=($ROS_DISTRO)
+
+sh ./scripts/docker_build_nocache.sh ros:rcraicer-latest Dockerfile.rcraicer --build-arg BASE_IMAGE=$BASE_IMAGE
